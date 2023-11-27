dca4143ccf1a00ce2b53e971f2cd29bdaf3387f8
kaizeth1 <151888590+kaizeth1@users.noreply.github.com>
2023-11-27 17:47:47 +0900

    Add files via upload

---
+++ src/main/java/com/aidata/fundingtrip/entity/Member.java
@@ -0,0 +1,29 @@
+package com.aidata.fundingtrip.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "member")
+@Data
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String mid;
+
+    @Column(nullable = false, length = 20)
+    private String mpw;
+
+    @Column(nullable = false, length = 20)
+    private String mph;
+
+    @Column(nullable = false, length = 20)
+    private String memail;
+
+    @Column(length = 20)
+    private String mgrade;
+
+    @Column(length = 30)
+    private String mpayment;
+
+}
