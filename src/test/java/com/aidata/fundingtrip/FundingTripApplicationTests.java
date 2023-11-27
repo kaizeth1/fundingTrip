d2ab81343f7c35adad1af1eec367ea4bb98e160c
yangsub kim <dudad@naver.com>
2023-11-27 10:54:04 +0900

    Initial commit

---
+++ src/test/java/com/aidata/fundingtrip/FundingTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aidata.fundingtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
