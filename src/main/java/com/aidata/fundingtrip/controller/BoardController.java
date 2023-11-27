700aada40af958f3852e1766942898b35994fb8c
kaizeth1 <151888590+kaizeth1@users.noreply.github.com>
2023-11-27 17:01:52 +0900

    Add files via upload

---
+++ src/main/java/com/aidata/fundingtrip/controller/BoardController.java
@@ -0,0 +1,68 @@
+package com.aidata.fundingtrip.controller;
+
+import com.aidata.fundingtrip.dto.HistoryBoardDto;
+import com.aidata.fundingtrip.dto.ListDto;
+import com.aidata.fundingtrip.dto.MemberDto;
+import com.aidata.fundingtrip.service.BoardService;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Slf4j
+public class BoardController {
+	private ModelAndView mv;
+	@Autowired
+	private BoardService bServ;
+
+	//메인 화면
+	@GetMapping("/")
+	public ModelAndView home(ListDto list, HttpSession session) {
+		log.info("home()");
+
+		return mv;
+	}
+
+	//마이 페이지
+	@GetMapping("myPage")
+	public String myPage(MemberDto member, HttpSession session) {
+		log.info("myPage()");
+
+		return "myPage";
+	}
+
+	//유적지 목록 화면
+	@GetMapping("historyList")
+	public String historyList(HistoryBoardDto history, HttpSession session) {
+		log.info("historyList()");
+
+		return "historyList";
+	}
+
+	//유적지 상세 보기
+	@GetMapping("historyDetail")
+	public ModelAndView historyDetail(int hisnum) {
+		log.info("historyDetail()");
+
+		return mv;
+	}
+
+	//펀딩 글 목록
+	@GetMapping("fundingList")
+	public String fundingList() {
+		log.info("fundingList()");
+
+		return "fundingList";
+	}
+
+	//고객 센터 상세 보기
+	@GetMapping("qnaDetail")
+	public ModelAndView qnaDetail(int qnum) {
+		log.info("qnaDetail()");
+
+		return mv;
+	}
+}
---
+++ src/main/java/com/aidata/fundingtrip/controller/MemberController.java
@@ -0,0 +1,68 @@
+package com.aidata.fundingtrip.controller;
+
+import com.aidata.fundingtrip.dto.MemberDto;
+import com.aidata.fundingtrip.service.BoardService;
+import com.aidata.fundingtrip.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@Slf4j
+public class MemberController {
+	private ModelAndView mv;
+	@Autowired
+	private BoardService bServ;
+	@Autowired
+	private MemberService mServ;
+
+	//로그인 화면
+	@GetMapping("loginForm")
+	public String loginForm() {
+		log.info("loginForm()");
+		return "loginForm";
+	}
+
+	//로그인 처리
+	@PostMapping("loginProc")
+	public String loginProc(MemberDto member, HttpSession session, RedirectAttributes rttr) {
+		log.info("loginProc");
+		String view = mServ.loginProc(member, session, rttr);
+		return view;
+	}
+
+	//아이디 찾기
+	@GetMapping("searchIdForm")
+	public ModelAndView searchIdForm(MemberDto member, HttpSession session) {
+		log.info("searchIdForm()");
+
+		return mv;
+	}
+	//아이디 찾기 처리
+//	@PostMapping("searchIdProc")
+//	public String searchIdProc(MemberDto member, HttpSession session) {
+//		log.info("searchIdProc()");
+//		String view = hServ =
+//		return view;
+//	}
+
+	//비밀번호 찾기
+	@GetMapping("searchPwForm")
+	public ModelAndView searchPwForm(MemberDto member, HttpSession session) {
+		log.info("searchPwForm()");
+
+		return mv;
+	}
+//	//비밀번호 찾기 처리
+//	@PostMapping("searchPwProc")
+//	public String searchPwProc(MemberDto member, HttpSession session) {
+//		log.info("searchPwProc()");
+//		String view = hServ =
+//		return view;
+//	}
+}
