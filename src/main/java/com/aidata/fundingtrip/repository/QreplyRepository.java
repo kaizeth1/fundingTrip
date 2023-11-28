d6b76866970b7c2deb9c0bbd366b827ded9e472e
kaizeth1 <151888590+kaizeth1@users.noreply.github.com>
2023-11-28 17:00:06 +0900

    Delete src directory

--- src/main/java/com/aidata/fundingtrip/repository/QreplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aidata.fundingtrip.repository;
-
-import com.aidata.fundingtrip.entity.Qreply;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Transactional
-public interface QreplyRepository extends JpaRepository<Qreply, Integer> {
-}
--- src/main/java/com/aidata/fundingtrip/repository/TreplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aidata.fundingtrip.repository;
-
-import com.aidata.fundingtrip.entity.Treply;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@Transactional
-public interface TreplyRepository extends JpaRepository<Treply, Integer> {
-}
