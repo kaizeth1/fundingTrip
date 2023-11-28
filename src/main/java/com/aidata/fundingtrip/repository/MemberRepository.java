b93ec303011b30355aa10b81559cd3b88c45f3b8
kaizeth1 <151888590+kaizeth1@users.noreply.github.com>
2023-11-28 11:44:22 +0900

    Delete src/main/java/com/aidata/fundingtrip/repository directory

--- src/main/java/com/aidata/fundingtrip/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aidata.fundingtrip.repository;
-
-import com.aidata.fundingtrip.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
